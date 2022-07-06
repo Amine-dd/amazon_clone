4c531d2c7669b248de5916774e29cc38c7a67889
Mohamed Amine Bouslimi <aminobouslimi@gmail.com>
2022-07-06 14:16:42 +0100

    added header

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <div className="App">
+      <header>
+        <a href="/">Amazon Clone</a>
+      </header>
+      <main>List products</main>
+    </div>
+  );
+}
+
+export default App;
