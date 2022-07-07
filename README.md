01195916d18378e74e3124f3151e8b542454cbc7
Amine-dd <aminobouslimi@gmail.com>
2022-07-07 11:48:16 +0100

    updated

--- README.md
+++ README.md
@@ -11,3 +11,8 @@
    2.add product images
    3.render products
    4.style products
+
+6.Add routing
+1.npm i react-router-dom
+2.create route for home screen
+3.create router fpr product screen
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,33 +1,21 @@
-import data from './data';
-
+import { BrowserRouter, Link, Route, Routes } from 'react-router-dom';
+import HomeScreen from './screens/HomeScreen';
+import ProductScreen from './screens/ProductScreen';
 function App() {
   return (
-    <div>
-      <header>
-        <a href="/">Amazon Clone</a>
-      </header>
-      <main>
-        <h1>Featured products</h1>
-        <div className="products">
-          {data.products.map((product) => (
-            <div className="product" key={product.slug}>
-              <a href={`/product/${product.slug}`}>
-                <img src={product.image} alt={product.name} />
-              </a>
-              <div className="product-info">
-                <a href={`/product/${product.slug}`}>
-                  <p>{product.name}</p>
-                </a>
-                <p>
-                  <strong>{product.price}</strong>
-                </p>
-                <button>Add to cart</button>
-              </div>
-            </div>
-          ))}
-        </div>
-      </main>
-    </div>
+    <BrowserRouter>
+      <div>
+        <header>
+          <Link to="/">Amazon Clone</Link>
+        </header>
+        <main>
+          <Routes>
+            <Route path="/" element={<HomeScreen />} />
+            <Route path="/product/:slug" element={<ProductScreen />} />
+          </Routes>
+        </main>
+      </div>
+    </BrowserRouter>
   );
 }
 
