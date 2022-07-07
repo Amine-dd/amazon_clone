33ee2cd33363d3002e0e2b718a098465ee38654e
Amine-dd <aminobouslimi@gmail.com>
2022-07-07 23:50:10 +0100

    fetch data f

---
+++ backend/data.js
@@ -0,0 +1,53 @@
+const data = {
+  products: [
+    {
+      name: 'Nike Slim shirt',
+      slug: 'nike-slim-shirt',
+      category: 'Shirts',
+      image: '/images/p1.jpg',
+      price: 120,
+      countInStock: 10,
+      rating: 4.5,
+      numReviews: 10,
+      description: 'high quality shirt',
+    },
+
+    {
+      name: 'Adidas Slim shirt',
+      slug: 'adidas-slim-shirt',
+      category: 'Shirts',
+      image: '/images/p3.jpg',
+      price: 120,
+      countInStock: 10,
+      rating: 4.5,
+      numReviews: 10,
+      description: 'high quality shirt',
+    },
+
+    {
+      name: 'Reebok Slim pants',
+      slug: 'Reebok-slim-pants',
+      category: 'Pants',
+      image: 'images/p4.jpg',
+      price: 180,
+      countInStock: 20,
+      rating: 4.9,
+      numReviews: 50,
+      description: 'high quality Pants Reebok',
+    },
+
+    {
+      name: 'Adidas Slim pants',
+      slug: 'Adidas slim pants',
+      category: 'Pants',
+      image: '/images/p2.jpg',
+      price: 60,
+      countInStock: 110,
+      rating: 4.8,
+      numReviews: 60,
+      description: 'very good quality pants',
+    },
+  ],
+};
+
+export default data;
